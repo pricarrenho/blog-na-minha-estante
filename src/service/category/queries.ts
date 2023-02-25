7df5c22a05b574fbb1323ac8ead1740ba11b3f05
Priscilla Carrenho <priscillaonil@gmail.com>
2023-02-25 20:29:15 -0300

    fix: services file extension

--- src/service/category/queries.ts
+++ src/service/category/queries.ts
@@ -3,7 +3,14 @@ import { gql } from "graphql-request";
 export const GET_CATEGORY = gql`
   query getCategory($category: String) {
     category(where: { slug: $category }) {
-      id
+      name
+      subcategories {
+        name
+      }
+      post {
+        slug
+        title
+      }
     }
   }
 `;
