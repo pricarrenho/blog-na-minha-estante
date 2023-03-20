242f5b1b14068193feb8547ead5336d27c47b2e3
Priscilla Carrenho <priscillaonil@gmail.com>
2023-03-20 14:16:32 -0300

    feat: add enviromment

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,5 +1,5 @@
 import { GraphQLClient } from "graphql-request";
 
-export const api = new GraphQLClient(
-  "https://sa-east-1.cdn.hygraph.com/content/clee7h8o75ehj01t6ebjb1lz7/master"
-);
+const url = process.env.NEXT_PUBLIC_GRAPHQL_URL as string;
+
+export const api = new GraphQLClient(url);
