1ba464462de1ef06ffdae07449e2f4442a2800e1
Priscilla Carrenho <priscillaonil@gmail.com>
2023-04-03 11:19:59 -0300

    style: styles fixed

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -8,8 +8,8 @@ function Footer() {
       <Container>
         <S.Content>
           <S.ContentText>
-            Na Minha Estante• Todos os direitos reservados • Desenvolvido com 💜
-            por{" "}
+            Na Minha Estante • Todos os direitos reservados • Desenvolvido com
+            💜 por{" "}
             <span>
               <Link href={"https://pricarrenho.com.br"} target="_blank">
                 <b>Priscilla Carrenho</b>
