75676ded82f1e121e6247c8747529977869e3fed
Priscilla <priscillaonil@gmail.com>
2023-06-26 14:19:30 -0300

    feat: added link in section titles

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -8,6 +8,7 @@ import SimpleSlider from "components/SimpleSlider";
 import Title from "components/Title";
 import TitleHome from "components/TitleHome";
 import * as S from "./styles";
+import Link from "next/link";
 
 export function HomeTemplate() {
   const { data } = useSWR("/api/posts", getPosts);
@@ -28,14 +29,20 @@ export function HomeTemplate() {
       </S.CustomContainer>
 
       <Container>
-        <Title>Resenhas</Title>
+        <Link href="/contos">
+          <Title>Contos</Title>
+        </Link>
+        <CardPostsHome item={contos} />
+
+        <Link href="/resenhas">
+          <Title>Resenhas</Title>
+        </Link>
         <CardPostsHome item={resenhas} />
 
-        <Title>Autores</Title>
+        <Link href="/autores">
+          <Title>Autores</Title>
+        </Link>
         <CardPostsHome item={autores} />
-
-        <Title>Contos</Title>
-        <CardPostsHome item={contos} />
       </Container>
       <Footer />
     </main>
