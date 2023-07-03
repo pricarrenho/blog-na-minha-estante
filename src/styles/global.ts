04732e5891d162ab85ad2821f7b26c00d8b8d789
Priscilla <priscillaonil@gmail.com>
2023-07-03 17:54:06 -0300

    feat: improve accessibility

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,6 +4,7 @@ import { Poppins } from "@next/font/google";
 const poppins = Poppins({
   weight: ["400", "500", "600", "700", "800", "900"],
   subsets: ["latin"],
+  display: "swap",
 });
 
 export const GlobalStyle = createGlobalStyle`
