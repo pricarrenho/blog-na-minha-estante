444e69ee391ac0bad8191f4b5f19c2d2b9fb85dd
Priscilla <priscillaonil@gmail.com>
2024-06-10 21:45:58 -0300

    feat: add new font family to title

--- src/components/TitleHome/styles.ts
+++ src/components/TitleHome/styles.ts
@@ -1,12 +1,19 @@
 import styled, { css } from "styled-components";
+import { Gloria_Hallelujah } from "@next/font/google";
+
+const gloria = Gloria_Hallelujah({
+  weight: ["400"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const Wrapper = styled.div`
   ${({ theme }) => css`
     text-align: center;
-    padding: 48px 0 96px;
+    padding: 48px 0 110px;
 
     @media (min-width: 800px) {
-      padding: 108px 0 160px;
+      padding: 90px 0 160px;
     }
   `}
 `;
@@ -18,11 +25,12 @@ export const Title = styled.h1`
     font-weight: 600;
     color: ${theme.colors.primary[50]};
     margin-bottom: 16px;
+    font-family: ${gloria.style.fontFamily};
 
     @media (min-width: 800px) {
       font-size: 48px;
       max-width: 900px;
-      margin: 0 auto 16px;
+      margin: 0 auto 32px;
     }
   `}
 `;
@@ -30,16 +38,15 @@ export const Title = styled.h1`
 export const Subtitle = styled.h2`
   ${({ theme }) => css`
     font-size: ${theme.font.sizes.sm};
-    line-height: 20px;
+    line-height: 24px;
     font-weight: 300;
-    max-width: 280px;
     margin: 0 auto;
     color: ${theme.colors.neutral[800]};
 
     @media (min-width: 800px) {
-      font-size: 18px;
-      line-height: 26px;
-      max-width: 660px;
+      font-size: 20px;
+      line-height: 30px;
+      max-width: 790px;
     }
   `}
 `;
