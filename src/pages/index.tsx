e203721c4be93f98821ebc8f46829464ffe8e846
Priscilla <priscillaonil@gmail.com>
2024-06-10 21:54:07 -0300

    feat: featch home api/post

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { getCategories } from "service/category/getCategories";
 import { HomeTemplate } from "templates/Home";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { getPosts } from "service/post/getPosts";
 
 export default function Home() {
   return <HomeTemplate />;
@@ -9,12 +10,14 @@ export default function Home() {
 
 export async function getStaticProps() {
   const categories = await getCategories();
+  const posts = await getPosts();
 
   return {
     revalidate: 60,
     props: {
       fallback: {
         "/api/categories": categories,
+        "/api/posts": posts,
       },
     },
   };
